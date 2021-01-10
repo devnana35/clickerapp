82e3230b43e8fd4e64981c86622a0cf524c8ab9e
devnana35 <devnana35@gmail.com>
2021-01-10 14:30:30 +0700

    first vue

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <h2>{{ state.count }}, doubled = {{ state.double }}</h2>
+  <button @click="UpdateCount(true)">Increase Value</button>
+  <button @click="UpdateCount(false)">Decrease Value</button>
+</template>
+
+<script>
+import { reactive, computed } from 'vue';
+
+export default {
+  setup() {
+    const state = reactive ({
+      count: 0,
+      double: computed(() => state.count * 2)
+    });
+
+    function UpdateCount(increase) {
+      if(increase)
+        state.count++;
+      else
+        state.count--;
+    }
+
+    return {
+      state,
+      UpdateCount,
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
